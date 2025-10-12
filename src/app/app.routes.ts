8a9443caaaa663205736d0287c39109c160cd6c6
Eduardo Matuella <eduardomatuella@gmail.com>
2025-10-12 20:49:13 -0300

    Definição de rotas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
+import { AuthGuard } from './features/auth/auth-guard';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard';
+import { ProjectListComponent } from './features/projects/project-list/project-list';
+import { ProjectDetailComponent } from './features/projects/project-detail/project-detail';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent, // Um layout com header e sidebar
+    canActivate: [AuthGuard],      // Protege todas as rotas filhas
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'projetos', component: ProjectListComponent },
+      { path: 'projetos/:id', component: ProjectDetailComponent },
+    ]
+  },
+  { path: '**', redirectTo: 'login' } // Rota coringa
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
